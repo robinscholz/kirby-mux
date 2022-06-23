6eebd0f32151a32b65540db5aa232b238bda79d8
Robin Scholz <dev@studioscholz.info>
2022-06-23 11:32:13 +0200

    fix: env handling

--- classes/KirbyMux/Methods.php
+++ classes/KirbyMux/Methods.php
@@ -7,7 +7,7 @@ use MuxPhp;
 class Methods
 {
     public static function upload($assetsApi, $url) {
-        $file = getenv('MUX_DEV') ? "https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4" : $url;
+        $file = $_ENV['MUX_DEV'] ? "https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4" : $url;
         $input = new MuxPhp\Models\InputSettings(["url" => $file]);
         $createAssetRequest = new MuxPhp\Models\CreateAssetRequest(["input" => $input, "playback_policy" => [MuxPhp\Models\PlaybackPolicy::_PUBLIC] ]);
         $result = $assetsApi->createAsset($createAssetRequest);
